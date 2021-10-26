ffb209f7cfbf657c8011aac5eabd4c93b23d9e11
antoshkow <antoshkow1703@gmail.com>
2021-10-26 17:17:08 +0300

    fix: server

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
-const app = express();
+const favicon = require('express-favicon');
 const path = require('path');
+
+const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static('public'));
+app.use(favicon(__dirname + '/build/favicon.ico'));
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 app.listen(port, () =>
   console.log(`Server is running on: http://localhost:${port}`));
